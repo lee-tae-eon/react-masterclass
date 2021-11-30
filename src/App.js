0ba52ab6309c267759f36db510b58c35cb8362b9
lee-tae-eon <mktears0609@gmail.com>
2021-11-30 22:04:30 +0900

    2.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+
+const Box = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+
+const Text = styled.span`
+  color: white;
+`;
+
 function App() {
-  return null;
+  return (
+    <Father>
+      <Box>
+        <Text>Hello</Text>
+      </Box>
+      <BoxTwo></BoxTwo>
+    </Father>
+  );
 }
 
 export default App;
