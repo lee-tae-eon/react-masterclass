f0c30cb4afa8ae8de7b8e0c8d28d6ad5cb1d3763
lee-tae-eon <mktears0609@gmail.com>
2021-12-01 21:53:28 +0900

    2.5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 const Father = styled.div`
   display: flex;
@@ -8,14 +8,42 @@ const Input = styled.input.attrs({ required: true })`
   background-color: tomato;
 `;
 
+const animation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  } 50% {
+    transform: rotate(360deg);
+    border-radius: 100px;
+  } 100% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+`;
+
+const Box = styled.div`
+  width: 200px;
+  height: 200px;
+  background-color: tomato;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${animation} 2s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 50px;
+    }
+  }
+`;
+
 function App() {
   return (
     <Father>
       <Input />
-      <Input />
-      <Input />
-      <Input />
-      <Input />
+      <Box>
+        <span>✅</span>
+      </Box>
     </Father>
   );
 }
