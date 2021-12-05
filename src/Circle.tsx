928c12a05ed0bcc6841687a7972e7f408895c68a
lee-tae-eon <mktears0609@gmail.com>
2021-12-05 15:15:11 +0900

    3.4

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
 }
 
 const Container = styled.div<CircleProps>`
@@ -9,10 +10,11 @@ const Container = styled.div<CircleProps>`
   height: 200px;
   background-color: ${(props) => props.bgColor};
   border-radius: 100px;
+  border: 5px solid ${(props) => props.borderColor};
 `;
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container bgColor={bgColor} />;
+function Circle({ bgColor, borderColor }: CircleProps) {
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 
 export default Circle;
