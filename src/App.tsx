ad3651ac051b0727875487e8970bd57e1efdf543
lee-tae-eon <mktears0609@gmail.com>
2021-12-06 21:58:51 +0900

    3.8

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,19 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  background-color: ${(props) => props.theme.bgColor};
+`;
+
+const H1 = styled.h1`
+  color: ${(props) => props.theme.textColor};
+`;
 
 function App() {
-  const [value, setValue] = useState("");
-
-  const onChange = (event: FormEvent<HTMLInputElement>) => {
-    const {
-      currentTarget: { value },
-    } = event;
-    setValue(value);
-  };
-
-  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    console.log("hello", value);
-  };
-
   return (
     <div>
-      <form onSubmit={onSubmit}>
-        <input
-          value={value}
-          onChange={onChange}
-          type="text"
-          placeholder="username"
-        />
-        <button>Log in</button>
-      </form>
+      <Container>
+        <H1>Protect</H1>
+      </Container>
     </div>
   );
 }
