04e5817fe0a911498a37a66d6d53df616be89181
lee-tae-eon <mktears0609@gmail.com>
2021-12-08 22:25:07 +0900

    4.2

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,17 +1,77 @@
-import { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+const Container = styled.div`
+  padding: 0 20px;
+`;
+
+const Header = styled.header`
+  height: 10vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CoinList = styled.ul``;
+
+const Coin = styled.li`
+  background-color: white;
+  color: ${(props) => props.theme.bgColor};
+  padding: 20px;
+  border-radius: 20px;
+  margin-bottom: 10px;
+`;
+
 const Title = styled.h1`
+  font-size: 48px;
   color: ${(props) => props.theme.accentColor};
 `;
 
+const coins = [
+  {
+    id: "btc-bitcoin",
+    name: "BitCoin",
+    symbol: "BTC",
+    rank: 1,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "eth-ethereum",
+    name: "Ethereum",
+    symbol: "ETH",
+    rank: 2,
+    is_new: false,
+    is_active: true,
+    type: "coin",
+  },
+  {
+    id: "hex-hex",
+    name: "HEX",
+    symbol: "HEX",
+    rank: 3,
+    is_new: false,
+    is_active: true,
+    type: "token",
+  },
+];
+
 function Coins() {
-  const [coins, setCoins] = useState<string>("");
   return (
-    <div>
-      test 중입니다.
-      <Title>Coins</Title>
-    </div>
+    <Container>
+      <Header>
+        <Title>코인</Title>
+      </Header>
+      <CoinList>
+        {coins.map((coin: any) => (
+          <Coin key={coin.id}>
+            {coin.name}
+            <Link to={`/${coin.id}`}>&rarr;</Link>
+          </Coin>
+        ))}
+      </CoinList>
+    </Container>
   );
 }
 
