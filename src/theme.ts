a54b860c40d9d11b044d29b78d5d7b59af12788f
lee-tae-eon <mktears0609@gmail.com>
2021-12-13 22:43:25 +0900

    4.9

--- src/theme.ts
+++ src/theme.ts
@@ -2,6 +2,6 @@ import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
   bgColor: "#2f3640",
-  textColor: "#3498db",
+  textColor: "white",
   accentColor: "#9c88ff",
 };
