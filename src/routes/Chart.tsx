506ce4d3b473cf650716fb82b81d4ea8aee9a531
lee-tae-eon <mktears0609@gmail.com>
2021-12-15 23:06:40 +0900

    4.13 7:24

--- src/routes/Chart.tsx
+++ src/routes/Chart.tsx
@@ -1,15 +1,56 @@
 import { useQuery } from "react-query";
 import { fetchCoinHistory } from "../api";
+import ApexChart from "react-apexcharts";
+
+interface IHistorical {
+  time_open: string;
+  time_close: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  volume: number;
+  market_cap: number;
+}
 
 interface ChartProps {
   coinId: string;
 }
 
 function Chart({ coinId }: ChartProps) {
-  const { isLoading, data } = useQuery(["ohlcv", coinId], () =>
+  const { isLoading, data } = useQuery<IHistorical[]>(["ohlcv", coinId], () =>
     fetchCoinHistory(coinId)
   );
 
-  return <h1>Chart</h1>;
+  return (
+    <div>
+      {isLoading ? (
+        "Loading chart...."
+      ) : (
+        <ApexChart
+          type="line"
+          series={[
+            {
+              name: "hello",
+              data: [1, 2, 3, 4, 5, 6],
+            },
+            {
+              name: "sales",
+              data: [15, 18, 15, 78, 99, 22],
+            },
+          ]}
+          options={{
+            theme: {
+              mode: "dark",
+            },
+            chart: {
+              height: 500,
+              width: 500,
+            },
+          }}
+        />
+      )}
+    </div>
+  );
 }
 export default Chart;
