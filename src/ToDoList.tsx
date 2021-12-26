0411d2793fee1d5af638b10c2604197fed1f6792
lee-tae-eon <mktears0609@gmail.com>
2021-12-26 20:16:11 +0900

    error

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,31 +1,5 @@
-import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 
-// function ToDoList() {
-//   const [toDo, setTodo] = useState("");
-//   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
-//     const {
-//       currentTarget: { value },
-//     } = event;
-//     if (toDo.length < 10) {
-
-//     }
-//   };
-
-//   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-//     event.preventDefault();
-//     console.log(toDo);
-//   };
-
-//   return (
-//     <div>
-//       <form onSubmit={onSubmit}>
-//         <input onChange={onChange} value={toDo} placeholder="Write a to do" />
-//         <button>Add</button>
-//       </form>
-//     </div>
-//   );
-// }
 interface TodoFormProps {
   toDo: string;
 }
@@ -34,8 +8,7 @@ const ToDoList = () => {
   const { register, getValues, watch, handleSubmit } = useForm<TodoFormProps>({
     mode: "onChange",
   });
-  const test = "test";
-  const test1 = "test1";
+
   const { toDo } = watch();
 
   const onValid = (data: any) => {
@@ -45,7 +18,15 @@ const ToDoList = () => {
   console.log(toDo);
 
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        marginTop: "50px",
+      }}
+    >
       <form onSubmit={handleSubmit(onValid)}>
         <input {...register("toDo")} placeholder="Write a to do" />
         <button>Add</button>
