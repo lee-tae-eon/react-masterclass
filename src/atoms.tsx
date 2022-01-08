8e63217fdc8256ca59b154d408778a2bbd24af1a
lee-tae-eon <mktears0609@gmail.com>
2022-01-08 22:55:20 +0900

    5.17

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -8,13 +8,17 @@ export interface IToDo {
 
 export const toDoState = atom<IToDo[]>({
   key: "toDo",
-
   default: [],
 });
 
 export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
-    return "Hello";
+    const toDos = get(toDoState);
+    return [
+      toDos.filter((toDo) => toDo.category === "TO_DO"),
+      toDos.filter((toDo) => toDo.category === "DOING"),
+      toDos.filter((toDo) => toDo.category === "DONE"),
+    ];
   },
 });
--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -4,16 +4,33 @@ import CreateToDo from "./CreateToDo";
 import ToDo from "./ToDo";
 
 function ToDoList() {
-  const toDos = useRecoilValue(toDoState);
-  const selectorOutPut = useRecoilValue(toDoSelector);
-  console.log(selectorOutPut);
+  const [toDos, doing, done] = useRecoilValue(toDoSelector);
+
   return (
     <div>
       <h1>To dos</h1>
       <hr />
       <CreateToDo />
+      <h2>TO DO</h2>
+      <hr />
       <ul>
         {toDos.map((todo) => (
+          <ToDo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <br />
+      <h2>DOING</h2>
+      <hr />
+      <ul>
+        {doing.map((todo) => (
+          <ToDo key={todo.id} {...todo} />
+        ))}
+      </ul>
+      <br />
+      <h2>DONE</h2>
+      <hr />
+      <ul>
+        {done.map((todo) => (
           <ToDo key={todo.id} {...todo} />
         ))}
       </ul>
