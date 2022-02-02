91270d74ffb8675588be94092c3abeb2d3001cc8
lee-tae-eon <mktears0609@gmail.com>
2022-02-02 22:18:20 +0900

    slider 7.12

--- src/App.tsx
+++ src/App.tsx
@@ -3,57 +3,77 @@ import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
-  max-width: 100vw;
+  width: 100%;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   background: linear-gradient(135deg, #e09, #d0e);
 `;
 
 const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
+  width: 100px;
+  height: 100px;
   background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
+  border-radius: 10px;
   position: absolute;
-  top: 100px;
+  top: 200px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 20px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial: {
+const boxVariant = {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
   visible: {
+    x: 0,
     opacity: 1,
     scale: 1,
-    rotateZ: 360,
+    transition: {
+      duration: 0.5,
+    },
   },
-  leaving: {
+  exit: {
+    x: -500,
     opacity: 0,
     scale: 0,
-    y: 10,
+    transition: {
+      duration: 0.5,
+    },
   },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+
+  const nextPlease = () => setVisible((prev) => (prev === 10 ? 1 : prev + 1));
+  const prevPlease = () => setVisible((prev) => (prev === 1 ? 10 : prev - 1));
   return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
       <AnimatePresence>
-        {showing ? (
-          <Box
-            variants={boxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
-        ) : null}
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
+          i === visible ? (
+            <Box
+              variants={boxVariant}
+              initial="invisible"
+              animate="visible"
+              exit="exit"
+              key={i}
+            >
+              {i}
+            </Box>
+          ) : null
+        )}
       </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prevPlease}>prev</button>
     </Wrapper>
   );
 }
