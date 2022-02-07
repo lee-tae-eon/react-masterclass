d4f852a40d2f1e23772b310e51101dea8bef36b6
lee-tae-eon <mktears0609@gmail.com>
2022-02-07 23:35:33 +0900

    grid

--- src/App.tsx
+++ src/App.tsx
@@ -3,50 +3,41 @@ import { motion } from "framer-motion";
 import { useState } from "react";
 
 const Wrapper = styled(motion.div)`
-  width: 100%;
+  width: 100vw;
   height: 100vh;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  gap: 20px;
   background: linear-gradient(135deg, #e09, #d0e);
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 10px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
 `;
 
-const Circle = styled(motion.div)`
-  height: 50px;
-  width: 50px;
-
-  background-color: #00a5ff;
+const Box = styled(motion.div)`
+  height: 200px;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-
-  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box>
-        {!clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 50, scale: 2 }} />
-        ) : null}
-      </Box>
-      <Box>
-        {clicked ? (
-          <Circle layoutId="circle" style={{ borderRadius: 0 }} />
-        ) : null}
-      </Box>
+    <Wrapper>
+      <Grid>
+        <Box />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
     </Wrapper>
   );
 }
