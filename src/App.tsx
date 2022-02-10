db47a84ae4df98329ce7cf26bbaee0f77e3252fe
lee-tae-eon <mktears0609@gmail.com>
2022-02-10 21:56:19 +0900

    7.16

--- src/App.tsx
+++ src/App.tsx
@@ -40,15 +40,13 @@ const Overlay = styled(motion.div)`
 `;
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggle = () => setClicked((prev) => !prev);
+  const [id, setId] = useState<string | null>(null);
   return (
-    <Wrapper onClick={toggle}>
+    <Wrapper>
       <Grid>
-        <Box layoutId="hello" />
-        <Box />
-        <Box />
-        <Box />
+        {[1, 2, 3, 4].map((n) => (
+          <Box key={n} layoutId={n + ""} />
+        ))}
       </Grid>
       <AnimatePresence>
         {clicked ? (
