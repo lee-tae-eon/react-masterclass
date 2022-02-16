29efd0d25044569ec675b430fcc9446aae3cc3b4
lee-tae-eon <mktears0609@gmail.com>
2022-02-16 22:22:07 +0900

    8.2

--- src/Routes/Components/Header.tsx
+++ src/Routes/Components/Header.tsx
@@ -1,14 +1,49 @@
 import styled from "styled-components";
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  width: 100%;
+  height: 80px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  background-color: red;
+  font-size: 16px;
+`;
 
-const Col = styled.div``;
+const Col = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Logo = styled.svg`
+  margin-right: 50px;
+`;
+
+const Items = styled.ul`
+  display: flex;
+  align-items: center;
+`;
+
+const Item = styled.li`
+  margin-right: 20px;
+`;
 
 function Header() {
   return (
     <Nav>
-      <Col></Col>
-      <Col></Col>
+      <Col>
+        <Logo />
+        <Items>
+          <Item>Home</Item>
+          <Item>TV Show</Item>
+        </Items>
+      </Col>
+      <Col>
+        <button>Search</button>
+      </Col>
       <Col></Col>
     </Nav>
   );
