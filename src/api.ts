e8c854bae64cdd0bd38852c974a1818edc2ecc66
lee-tae-eon <mktears0609@gmail.com>
2022-03-13 21:55:51 +0900

    add fetch function

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,8 @@
-export const API_KEY = "6c2a18c3a603a48a447e48c59ffd5b1a";
+const API_KEY = "6c2a18c3a603a48a447e48c59ffd5b1a";
+const BASE_PATH = "https://api.themoviedb.org/3";
+
+export function getMovies() {
+  return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
+    (res) => res.json()
+  );
+}
