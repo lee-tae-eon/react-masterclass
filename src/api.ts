14b577567f85ea95de6181bee58dc94f02e40b7e
lee-tae-eon <mktears0609@gmail.com>
2022-03-14 21:59:11 +0900

    type definition

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,25 @@
 const API_KEY = "6c2a18c3a603a48a447e48c59ffd5b1a";
 const BASE_PATH = "https://api.themoviedb.org/3";
 
+interface IMovie {
+  id: number;
+  backdrop_path: string;
+  poster_path: string;
+  title: string;
+  overview: string;
+}
+
+export interface IGetMoviesResult {
+  dates: {
+    maximum: string;
+    minimum: string;
+  };
+  page: number;
+  result: IMovie[];
+  total_pages: number;
+  total_results: number;
+}
+
 export function getMovies() {
   return fetch(`${BASE_PATH}/movie/now_playing?api_key=${API_KEY}`).then(
     (res) => res.json()
