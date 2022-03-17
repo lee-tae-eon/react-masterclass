c2535d3871287c62b131aa98992c181b290ed017
lee-tae-eon <mktears0609@gmail.com>
2022-03-17 21:14:48 +0900

    8.10 start

--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -4,113 +4,115 @@ import { motion, AnimatePresence } from "framer-motion";
 import { getMovies, IGetMoviesResult } from "../api";
 import { makeImgPath } from "../utils";
 import { useState } from "react";
-
 const Wrapper = styled.div`
-  background-color: black;
+  background: black;
+  padding-bottom: 200px;
 `;
-
 const Loader = styled.div`
   height: 20vh;
   display: flex;
   justify-content: center;
   align-items: center;
 `;
-
-interface IBanner {
-  bgImg: string;
-}
-
-const Banner = styled.div<IBanner>`
-  width: 100vw;
+const Banner = styled.div<{ bgPhoto: string }>`
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   padding: 60px;
   background-image: linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-    url(${(props) => props.bgImg});
+    url(${(props) => props.bgPhoto});
   background-size: cover;
-  background-position: center center;
 `;
-
 const Title = styled.h2`
-  font-size: 58px;
-  margin-bottom: 20px;
+  font-size: 68px;
+  margin-bottom: 20px; ;
 `;
-
 const Overview = styled.p`
-  font-size: 24px;
+  font-size: 30px;
   width: 50%;
 `;
-
 const Slider = styled.div`
   position: relative;
   top: -100px;
 `;
-
 const Row = styled(motion.div)`
-  width: 100%;
   display: grid;
   gap: 5px;
   grid-template-columns: repeat(6, 1fr);
   position: absolute;
+  width: 100%;
 `;
-
 const Box = styled(motion.div)<{ bgPhoto: string }>`
-  background-color: #fff;
+  background-color: white;
   background-image: url(${(props) => props.bgPhoto});
   background-size: cover;
   background-position: center center;
   height: 200px;
-  color: red;
+  font-size: 66px;
+  &:first-child {
+    transform-origin: center left;
+  }
+  &:last-child {
+    transform-origin: center right;
+  }
 `;
 
 const rowVariants = {
   hidden: {
-    x: window.innerWidth + 5,
+    x: window.outerWidth + 5,
   },
   visible: {
     x: 0,
   },
   exit: {
-    x: -window.innerWidth - 5,
+    x: -window.outerWidth - 5,
+  },
+};
+
+const boxVariants = {
+  normal: {
+    scale: 1,
+  },
+  hover: {
+    scale: 1.3,
+    y: -50,
+    transition: {
+      delay: 0.5,
+      duaration: 0.3,
+      type: "tween",
+    },
   },
 };
 
 const offset = 6;
 
 function Home() {
-  const [index, setIndex] = useState(0);
-  const [leaving, setLeaving] = useState(false);
   const { data, isLoading } = useQuery<IGetMoviesResult>(
     ["movies", "nowPlaying"],
     getMovies
   );
-
-  const increaseIndex = () => {
+  const [index, setIndex] = useState(0);
+  const [leaving, setLeaving] = useState(false);
+  const incraseIndex = () => {
     if (data) {
       if (leaving) return;
-      setLeaving(true);
-      const totalCnt = data?.results.length - 1;
-      const maxIndex = Math.floor(totalCnt / offset) - 1;
-
+      toggleLeaving();
+      const totalMovies = data.results.length - 1;
+      const maxIndex = Math.floor(totalMovies / offset) - 1;
       setIndex((prev) => (prev === maxIndex ? 0 : prev + 1));
     }
   };
-
-  const toggleLeaving = () => {
-    setLeaving((prev) => !prev);
-  };
-
+  const toggleLeaving = () => setLeaving((prev) => !prev);
   return (
     <Wrapper>
       {isLoading ? (
-        <Loader>...Loading...</Loader>
+        <Loader>Loading...</Loader>
       ) : (
         <>
           <Banner
-            onClick={increaseIndex}
-            bgImg={makeImgPath(data?.results[0].backdrop_path || "")}
+            onClick={incraseIndex}
+            bgPhoto={makeImgPath(data?.results[0].backdrop_path || "")}
           >
             <Title>{data?.results[0].title}</Title>
             <Overview>{data?.results[0].overview}</Overview>
@@ -131,6 +133,10 @@ function Home() {
                   .map((movie) => (
                     <Box
                       key={movie.id}
+                      whileHover="hover"
+                      initial="normal"
+                      variants={boxVariants}
+                      transition={{ type: "tween" }}
                       bgPhoto={makeImgPath(movie.backdrop_path, "w500")}
                     />
                   ))}
@@ -142,5 +148,4 @@ function Home() {
     </Wrapper>
   );
 }
-
 export default Home;
